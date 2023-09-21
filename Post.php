124f831a8a93436e23de67de3dd26d929f1c4e5c
Felipe Wiaczorek <125615804+wiaczorekff@users.noreply.github.com>
2023-09-20 22:10:19 -0300

    models

---
+++ Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'content', 'category_id'];
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+}
